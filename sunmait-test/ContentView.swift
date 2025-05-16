096cc999a673149a05fa5bf4dbb10a3943af92dd
Bahdan Piatrouski <82778510+kefaxoo@users.noreply.github.com>
2025-05-17 01:36:14 +0300

    Initial Commit

---
+++ sunmait-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sunmait-test
+//
+//  Created by Bahdan Piatrouski on 17.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sunmait-test/sunmait_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  sunmait_testApp.swift
+//  sunmait-test
+//
+//  Created by Bahdan Piatrouski on 17.05.25.
+//
+
+import SwiftUI
+
+@main
+struct sunmait_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
